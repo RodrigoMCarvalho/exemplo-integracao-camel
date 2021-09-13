fb1b415b5379f97a440c21aabca1b0f327128b76
Rodrigo <rodrigo_moreira84@hotmail.com>
2021-09-13 18:11:25 -0300

    first commit

---
+++ ms-imovel/src/main/java/com/rodrigo/msimovel/MsImovelApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.msimovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsImovelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsImovelApplication.class, args);
+	}
+
+}
---
+++ ms-imovel/src/test/java/com/rodrigo/msimovel/MsImovelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.msimovel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsImovelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
